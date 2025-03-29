9c64bcc44d6b6a8ce02ca4e75d9a437f4f3bf96e
Jason Li <jligh145@gmail.com>
2025-03-29 13:19:11 -0400

    feat: signin/out + persist

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './app/screens/Login';
+import List from './app/screens/List';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useState } from 'react';
+import { firebase_auth } from './FirebaseConfig';
+import Extra from './app/screens/Extra';
 
-export default function App() {
+
+
+const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
+
+function InsideLayout() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <InsideStack.Navigator>
+      <InsideStack.Screen name='My todos' component={List}/>
+      <InsideStack.Screen name="Extras" component={Extra}/>
+      
+    </InsideStack.Navigator>
+  )
+}
+export default function App() {
+  const [user, setUser] = useState<User | null>(null)
+  
+  useEffect(() => {
+    onAuthStateChanged(firebase_auth, (user) => {
+      console.log('user', user);
+      setUser(user);
+    })
+  }, [])
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        {user ? (
+          <Stack.Screen name="Inside" component={InsideLayout} options={{ headerShown: false}}></Stack.Screen>
+        ) : (
+          <Stack.Screen name="Login" component={Login} options={{ headerShown: false}}></Stack.Screen>
+        )}
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ app/screens/List.tsx
@@ -0,0 +1,19 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+import { NavigationProp } from '@react-navigation/native';
+import { firebase_auth } from '../../FirebaseConfig';
+
+interface RouterProps{
+    navigation: NavigationProp<any,any>;
+}
+
+const List = ({ navigation } : RouterProps) => {
+  return (
+    <View>
+      <Button onPress={() => firebase_auth.signOut()} title="Sign Out"/>
+      <Button onPress={() => navigation.navigate('Extras')} title="Explore"/>
+    </View>
+  );
+};
+
+export default List
