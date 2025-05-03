ad01b4a19e64c6b1bc930eaf9c85dd72980626ba
Alan Yuan <100052449+AlanYuan16@users.noreply.github.com>
2025-05-02 20:23:15 -0400

    feat: timer and overlay

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -2,24 +2,17 @@ import { SafeAreaView, Text, Button, View, StyleSheet } from 'react-native'
 import React, { useContext } from 'react'
 import { NavigationProp } from '@react-navigation/native';
 import { firebase_auth } from '../../FirebaseConfig';
-import { OverlayContext } from './components/OverlayContext'; 
 
 interface RouterProps {
   navigation: NavigationProp<any, any>;
 }
 
 const List = ({ navigation }: RouterProps) => {
-  const { toggleOverlay } = useContext(OverlayContext);
 
   return (
     <SafeAreaView>
       <Button onPress={() => firebase_auth.signOut()} title="Sign Out" />
       <Button onPress={() => navigation.navigate('Home')} title="Explore" />
-      <Button 
-        onPress={toggleOverlay} 
-        title="Toggle Overlay" 
-        color="#8a2be2" //Purple color for the button
-      />
     </SafeAreaView>
   );
 };
